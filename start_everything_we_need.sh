281bd27abcbebc1d88a5cd0a02a02d459ac0095b
samirchaves <samirbraga@live.com>
2021-03-19 12:37:46 -0300

    finalizando script de inicialização

--- start_everything_we_need.sh
+++ start_everything_we_need.sh
@@ -2,9 +2,9 @@ echo "[]" > home_assistant/db/db.json
 docker start rabbit &&
 node home_assistant/index.js &
 sleep 2 &&
-(python3 actuators/equipament_server.py ar Ventinho 8080 &
-python3 actuators/equipament_server.py lampada Luz 8081 &
-python3 actuators/equipament_server.py sprinkler Chuveirinho 8082) &
-(python3 sensors/rabbitMQ/init_sensor.py smoke Fumacinha 2020 &
-python3 sensors/rabbitMQ/init_sensor.py light Farol 2020 &
-python3 sensors/rabbitMQ/init_sensor.py temperature Termômetro 2020)
+(python3 actuators/equipament_server.py ar "Ar-condicionado" 8080 &
+python3 actuators/equipament_server.py lampada "Lâmpada" 8081 &
+python3 actuators/equipament_server.py sprinkler "Sprinkler" 8082) &
+(python3 sensors/rabbitMQ/init_sensor.py smoke "Detector de Fumaça" 2020 &
+python3 sensors/rabbitMQ/init_sensor.py light "Sensor de Luminosidade" 2020 &
+python3 sensors/rabbitMQ/init_sensor.py temperature "Termômetro" 2020)
